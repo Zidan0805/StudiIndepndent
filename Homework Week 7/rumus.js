d6c2ad0289f09c0a30c6a0951d29316b4597d583
Muhammad Zidan Maulana <125979318+Zidan0805@users.noreply.github.com>
2023-09-30 21:33:19 +0700

    Add files via upload

---
+++ Homework Week 7/rumus.js
@@ -0,0 +1,16 @@
+exports.luasPersegi = function(sisi){
+    return sisi * sisi;
+};
+
+exports.kelilingPersegi = function(sisi){
+    return 4 * sisi;
+};
+
+exports.luasPersegiPanjang = function(panjang, lebar){
+    return panjang * lebar;
+};
+
+exports.kelilingPersegiPanjang = function(panjang, lebar){
+    return 2 * (panjang + lebar);
+};
+
