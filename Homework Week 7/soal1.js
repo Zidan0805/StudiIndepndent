afbc067847406ac7e7e4802c08f890bb1bea6ff9
Muhammad Zidan Maulana <125979318+Zidan0805@users.noreply.github.com>
2023-09-30 21:33:43 +0700

    Delete Homework Week 7 directory

--- Homework Week 7/soal1.js
+++
@@ -1,9 +0,0 @@
-const rumus = require('./rumus.js')
-
-console.log("Perhitungan Luas dan Keliling Persegi");
-console.log(rumus.luasPersegi(5));
-console.log(rumus.kelilingPersegi(10));
-
-console.log("Perhitungan Luas dan Keliling Persegi Panjang");
-console.log(rumus.luasPersegiPanjang(6, 7));
-console.log(rumus.kelilingPersegiPanjang(8, 3));
