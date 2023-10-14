fe31f09c466d44abdbecdce9fb5807891032b319
Muhammad Zidan Maulana <125979318+Zidan0805@users.noreply.github.com>
2023-10-15 01:02:49 +0700

    commit

--- index.js
+++ index.js
@@ -1,47 +1,81 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var pool = require('./queries.js');
+var db = require('./queries');
+var jwt = require('jsonwebtoken');
+var swaggerJsdoc = require('swagger-jsdoc');
+var swaggerUi = require('swagger-ui-express');
+var morgan = require('morgan');
+var port = 3000;
 
-app.get('/actor', (req, res) => {
-    pool.query('SELECT * FROM actor',(err, result) => {
-        if(err) {
-            throw err;
-        }
-        res.send(result.rows);
-    })
-})
+app.use(morgan('tiny'));
 
-app.get('/film', (req, res) => {
-    pool.query('SELECT * FROM film',(err, result) => {
-        if(err) {
-            throw err;
-        }
-        res.send(result.rows);
-    })
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/film/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    pool.query('SELECT * FROM film WHERE id = $1', [id], (err, result) => {
-        if(err) {
-            throw err;
-        }
-        res.send(result.rows);
-    })
-})
-
-app.get('/category', (req, res) => {
-    pool.query('SELECT * FROM category',(err, result) => {
-        if(err) {
-            throw err;
-        }
-        res.send(result.rows);
-    })
-})
-
-pool.connect((err, res) => {
-    console.log(err);
-    console.log('connected');
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
-app.listen(3000);
+app.get('/movies', db.getData);
+app.get('/movies/:id', db.getDataById);
+app.post('/movies', db.createData);
+app.put('/movies/:id', db.updateData);
+app.delete('/movies/:id', db.deleteData);
+app.get('/movies/?limit=5', db.paginateData);
+
+app.post('/register', (req, res) => {
+  const { email, gender, password, role } = req.body;
+  pool.query('INSERT INTO users (email, gender, password, role) VALUES ($1, $2)', [email, gender, password], role, (err, result) => {
+    if (err) {
+      res.status(500).send('Gagal mendaftar pengguna.');
+    } else {
+      res.status(201).send('Pengguna berhasil terdaftar.');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  pool.query('SELECT * FROM users WHERE email = $1', [email], (err, result) => {
+    if (err) {
+      res.status(500).send('Gagal saat mencoba login.');
+    } else {
+      const user = result.rows[0];
+      if (user && user.password === password) {
+        const token = jwt.sign({ email }, 'secret_key', { expiresIn: '1h' });
+        res.status(200).json({ token });
+      } else {
+        res.status(401).send(' Terjadi kesalahan.');
+      }
+    }
+  });
+});
+
+const option = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API menggunakan Swagger',
+      version: '0.1.0',
+      description:
+      'Simple CRUD API Application yang dibuat menggunakan Express dan Swagger',
+    },
+    server: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./queries.js'],
+};
+const specs = swaggerJsdoc(option);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
--- queries.js
+++ queries.js
@@ -1,10 +1,88 @@
+const { request, response } = require('express')
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'auliaagustin',
-    host: 'localhost',
-    database: 'dvdrental',
-    password: '',
-    port: 5432
+  user: 'auliaagustin',
+  host: 'localhost',
+  database: 'movies',
+  password: '',
+  port: 5432,
 })
+const getData = (request, response) => {
+  pool.query('SELECT * FROM movies ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-module.exports = pool;
+const getDataById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM movies WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createData = (request, response) => {
+  const { id, title, genres, year } = request.body
+
+  pool.query('INSERT INTO movies (id, title, genres, year) VALUES ($1, $2, $3)', [id, title, genres, year], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Data added with ID: ${results.insertId}`)
+  })
+}
+
+const updateData = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { title, genres, year } = request.body
+
+  pool.query(
+    'UPDATE movies SET title = $1, genres = $2, year = $3 WHERE id = $4',
+    [title, genres, year, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Data modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteData = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM movies WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Data deleted with ID: ${id}`)
+  })
+}
+
+const paginateData = (request, response) => {
+  pool.query(
+    `SELECT * FROM movies ${req.query.limit ? 'LIMIT ' + req.query.limit : ''}`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      results.json(results.rows);
+    }
+  );
+}
+
+module.exports = {
+  getData,
+  getDataById,
+  createData,
+  updateData,
+  deleteData,
+  paginateData
+}
